b7dcc1b50ecd338f50f51c01068b0760ddc26f97
Aceius E. <144858100+AceiusRedshift@users.noreply.github.com>
2026-08-01 13:11:25 -0700

    Update v0.8.0.0 (#7)
    * Change distribution model
    
    * Update metainfo + link
    
    * Ship flatpak-compatible build
    
    * Fix source-dir-not-allowed
    
    * Read me

--- simulatebuild.sh
+++ simulatebuild.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Note: This script does not update nuget-sources.json
-flatpak-builder build-dir --user --install-deps-from=flathub --download-only us.acei.AbsoluteInfinity.yaml 
-flatpak-builder build-dir --user --force-clean --install --repo=repo us.acei.AbsoluteInfinity.yaml
+flatpak-builder build-dir --user --install-deps-from=flathub --force-clean --install --repo=repo us.acei.AbsoluteInfinity.yaml
